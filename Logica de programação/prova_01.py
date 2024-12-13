a9af078900a10e2a5142ff294e8d9f6cba917e1b
ermessondev <ermessonr@icloud.com>
2024-12-12 21:06:49 -0300

    Add files via upload

---
+++ Logica de programação/prova_01.py
@@ -0,0 +1,18 @@
+# Exemplos de tipagens de variáveis em Python
+# Tipos básicos
+inteiro = 10  # Inteiro
+flutuante = 3.14  # Float
+texto = "Hello, World!"  # String
+booleano = True  # Booleano
+
+# Imprimindo os tipos das variáveis
+print(f"O tipo de 'inteiro' é: {type(inteiro)}")
+print(f"O tipo de 'flutuante' é: {type(flutuante)}")
+print(f"O tipo de 'texto' é: {type(texto)}")
+print(f"O tipo de 'booleano' é: {type(booleano)}")
+
+# Solicitação de dados ao usuário
+nome = input("Por favor, insira o seu nome: ")
+
+# Imprime a solicitação com uma mensagem personalizada
+print(f"Olá, {nome}! Seja bem-vindo ao nosso programa.")
